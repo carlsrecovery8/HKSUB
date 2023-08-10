b733f3bfbba282529492bbc464b44f9dc02ea073
carlsrecovery8 <135087150+carlsrecovery8@users.noreply.github.com>
2023-08-10 16:38:14 -0500

    Create TESTAPI.py

---
+++ TESTAPI.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3 
+
+import re 
+import colorama
+import request
+
+webside = "https://api.hackertarget.com"
+resultados = request.get(website)
+content = resultados.txt 
+print(content)
